279f75254f8361fe071aae0f23de05bfaff704da
Daviskelvin824 <daviskelvin824@gmail.com>
2025-09-28 11:22:54 +0700

    farcaster.json

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination:
+          "https://api.farcaster.xyz/miniapps/hosted-manifest/01998e89-0527-c0fb-6b8e-97fbbaafbd9c",
+        permanent: false, // false = 307 Temporary Redirect
+      },
+    ];
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
